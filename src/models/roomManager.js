020a8b329d97adfb1325c5f50b29911b692dfc63
DuvanSanchez12 <duvandcrs@gmail.com>
2026-02-22 20:13:57 -0500

    Cambios en GameHandler 6

--- src/models/roomManager.js
+++ src/models/roomManager.js
@@ -3,6 +3,7 @@ export class RoomManager {
     this.rooms = {};
   }
 
+  // Genera el código de 5 caracteres tal cual lo hacía el original
   generateCode() {
     const code = Math.random().toString(36).substring(2, 7).toUpperCase();
     return this.rooms[code] ? this.generateCode() : code;
@@ -12,12 +13,16 @@ export class RoomManager {
     return this.rooms[code];
   }
 
+  // Al crear la sala, inicializamos con los valores por defecto del original
   createRoom(hostId, hostName) {
     const code = this.generateCode();
     this.rooms[code] = {
       players: [{ id: hostId, name: hostName, role: "host" }],
       gameStarted: false,
-      settings: { maxPlayers: 5, timePerPerson: 60 },
+      settings: { 
+        maxPlayers: 5, 
+        timePerPerson: 60 
+      },
       turnData: null
     };
     return code;
@@ -27,6 +32,7 @@ export class RoomManager {
     delete this.rooms[code];
   }
 
+  // Buscador de sala por ID de socket (esencial para el disconnect)
   findRoomByPlayerId(playerId) {
     return Object.keys(this.rooms).find(code => 
       this.rooms[code].players.some(p => p.id === playerId)
